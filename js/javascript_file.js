3ae85e0deb5e7cb6296a24237c7e5d5ca727e1da
Victoria Aronow <varonow@mettel.net>
2020-01-20 20:39:00 -0500

    Add first JavaScript lesson

---
+++ js/javascript_file.js
@@ -0,0 +1,9 @@
+﻿var firstName = "Victoria", lastName="Aronow", city="NYC", state="NY";
+firstName = firstName.fontcolor("DarkBlue");
+lastName = lastName.fontcolor("orange");
+city = city.fontcolor("DarkRed");
+state= state.fontcolor("DarkGreen")
+
+
+document.write(firstName + " " + lastName + "<br />" + city + ", " + state);
+document.write("<br  />600 * 32 = " + (600 * 32));
