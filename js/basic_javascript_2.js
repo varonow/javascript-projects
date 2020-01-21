22d9abfc0846d9a36f7c53e5aeac630b5a3803c9
Victoria Aronow <varonow@mettel.net>
2020-01-20 21:39:13 -0500

    Add lesson 2

---
+++ js/basic_javascript_2.js
@@ -0,0 +1,7 @@
+function sayHi() {
+    var str = "Hello There!";
+    str  += "<br />";
+    str += "Welcome to my page";
+    document.getElementById("greeting").innerHTML = str.fontcolor("dodgerblue");
+}
+
