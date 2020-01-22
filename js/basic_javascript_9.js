088c91bd9dda0991c4496fa4dc22c9c66c4eb34e
Victoria Aronow <varonow@mettel.net>
2020-01-22 00:27:40 -0500

    Add Lesson 9

---
+++ js/basic_javascript_9.js
@@ -0,0 +1,49 @@
+/* timer code - changed a little to stop the counting!
+
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        if (seconds > -1) {
+            timer.innerHTML = seconds;
+            seconds = seconds - 1;
+        }
+
+        if(seconds == -1) {
+            alert("Time's up!");           
+        } else {
+            setTimeout(tick, 1000);
+        }
+    }
+
+    tick();  
+}
+*/
+
+// Slide Show
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (i=0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
